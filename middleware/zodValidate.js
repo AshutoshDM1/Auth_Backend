8b340031e43efa8301650f9e552b9df88138e797
AshutoshDM1 <downlodemaster1@gmail.com>
2024-05-21 17:09:26 +0530

    fix 3

--- middleware/zodValidate.js
+++ middleware/zodValidate.js
@@ -2,7 +2,6 @@ const validate = (registerSchema) => (req, res, next) => {
   console.log("Validating")
   try {
     const validate = registerSchema.safeParse(req.body);
-    console.log(validate)
     if (!validate.success) {
       return res.status(400).json({ error: "Validation failed Enter the correct values" });
   }
