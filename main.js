d1d3fef7f3e4531c1495617f208f1537b92cc533
AshutoshDM1 <downlodemaster1@gmail.com>
2024-05-21 10:26:17 +0530

    fix 1

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const {jwtSecret} = require('./config/index.js');
 app.use(bodyParser.json());
 app.use("/v1/user", userRoutes)
 
+app.get('/', (req, res) => {res.send('Hello World')});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
