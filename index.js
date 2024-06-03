752d3e49d9015f48d7c6d11a69ccc5a0af22d353
AshutoshDM1 <downlodemaster1@gmail.com>
2024-06-03 15:22:59 +0530

    installed cors

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const userRoutes = require('./routes/userRoutes.js');
 const postsRoutes = require('./routes/postsRoutes.js');
-const { rootdata } = require('./init/rootData.js')
+const { rootdata } = require('./init/rootData.js');
+const cors = require('cors');
 
+app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 app.use("/v1/user", userRoutes)
