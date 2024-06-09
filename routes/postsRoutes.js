105fdcc18fab0e3fc6ce36ebacb27f727970fcf0
AshutoshDM1 <downlodemaster1@gmail.com>
2024-06-09 18:03:37 +0530

    fix 1.4

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -15,7 +15,7 @@ const {
 
 router.get("/post", userValidate , getPosts);
 
-router.post("/post", userValidate ,zodValidate(PostSchema), createPost);
+router.post("/post", userValidate , createPost);
 
 router.delete("/deleteAllPosts", userValidate , deleteAllPosts);
 
